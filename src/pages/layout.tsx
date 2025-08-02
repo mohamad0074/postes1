9e0a07f8fcf41276296b691f283e500924509003
mohamad0074 <60906803+mohamad0074@users.noreply.github.com>
2025-08-02 20:22:04 +0700

    testing2

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,9 +1,8 @@
-import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
-export const metadata: Metadata = {
+export const Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
   generator: 'v0.dev',
